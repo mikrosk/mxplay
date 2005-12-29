f64c1cdd7a7e9b4dc610398fc4017664131f3002
Miro Kropacek <miro.kropacek@gmail.com>
2005-12-29 12:53:44 +0000

    added GPL headers to all [hcs] files

--- mxplay/dsp_fix.s
+++ mxplay/dsp_fix.s
@@ -1,8 +1,28 @@
 /*
- * dsp_fix.s
+ * dsp_fix.s -- custom Dsp_ExecProg
+ *
+ * Copyright (c) 2005 Miro Kropacek; mikro@hysteria.sk
+ * Copyright (c) 1998 NoCrew Laboratories.
  * 
- * COPYRIGHT (c) 1998 by NoCrew Laboratories.
- * 
+ * This file is part of the mxPlay project, multiformat audio player for
+ * Atari TT/Falcon computers.
+ *
+ * mxPlay is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mxPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mxPlay; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * Implements:
  * 
  *     extern Int dsp_load_program(Byte *program, Int length);
--- mxplay/misc.h
+++ mxplay/misc.h
@@ -1,3 +1,26 @@
+/*
+ * misc.h -- shared code, various topics (definitions and external declarations)
+ *
+ * Copyright (c) 2005 Miro Kropacek; mikro@hysteria.sk
+ * 
+ * This file is part of the mxPlay project, multiformat audio player for
+ * Atari TT/Falcon computers.
+ *
+ * mxPlay is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mxPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mxPlay; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef _MISC_H_
 #define _MISC_H_
 
